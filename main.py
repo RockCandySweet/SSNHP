d1344bdf92c285a0052a9cbf09d289cc25b50f1e
RockCandySweet <125380719+RockCandySweet@users.noreply.github.com>
2023-02-14 10:08:38 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Mario Vicente López Morales 1627823")
+
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
